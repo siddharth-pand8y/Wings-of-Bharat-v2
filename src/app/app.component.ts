26174406905e685f02c90bf20094bd313b53657f
Siddharth <siddharth.pand8y@gmail.com>
2019-12-17 22:56:41 +0530

    CSS Iteration #4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { WikipediaService } from './core/wikipedia/wikipedia.service';
 import { TimelineMax } from 'gsap';
@@ -8,20 +8,22 @@ import { TimelineMax } from 'gsap';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit { 
   title = 'wings-of-bharat';
   birdSanctuarySelection = new FormControl('');
   showInfoBox: boolean;
   listState: boolean;
   sactuaryDetail = {};
   animation = new TimelineMax({ paused: true, reversed: true });
+  dropdownStatus: boolean;
 
   constructor(private wikipediaService: WikipediaService) {
   }
 
   ngOnInit() {
     this.listState = true;
-    this.ListDropdownAnimation()
+    this.dropdownStatus = false;
+    this.ListDropdownAnimation();
   }
 
   selectSanctuary() {
@@ -32,11 +34,12 @@ export class AppComponent {
   }
 
   ListDropdownAnimation() {
-    this.animation.to(".sactuary-list-item", .5, { x: '-100vw', ease: "Expo.easeInOut" }, 0);
+    this.animation.to('.sactuary-list-item', .5, { x: '-100vw', ease: 'Expo.easeInOut' }, 0);
   }
 
   collapseList() {
     this.animation.reversed() ? this.animation.play() : this.animation.reverse();
     this.listState = !this.listState;
+    this.dropdownStatus = !this.dropdownStatus;
   }
 }
